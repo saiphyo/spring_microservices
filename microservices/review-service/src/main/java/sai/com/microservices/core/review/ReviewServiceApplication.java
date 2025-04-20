54f2b9937b580d3df645cf14afba6a72cc889d67
saiphyo <saiphyo@gmail.com>
2025-04-20 07:24:41 +0000

    Added api and util

--- microservices/review-service/src/main/java/sai/com/microservices/core/review/ReviewServiceApplication.java
+++ microservices/review-service/src/main/java/sai/com/microservices/core/review/ReviewServiceApplication.java
@@ -2,8 +2,10 @@ package sai.com.microservices.core.review;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("sai.com")
 public class ReviewServiceApplication {
 
 	public static void main(String[] args) {
